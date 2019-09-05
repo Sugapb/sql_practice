7eb1d81151e50a6a8ff56cf20e5ddb148c62f6e6
Sugapb <andreyritter@rambler.ru>
2019-09-05 11:30:16 +1100

    Update Solution.sql

--- Solution.sql
+++ Solution.sql
@@ -77,14 +77,19 @@ FROM pc a INNER JOIN pc b ON a.speed = b.speed AND a.ram = b.ram AND a.model > b
 
 Задание № 17:
 /* Найдите модели ПК-блокнотов, скорость которых меньше скорости любого из ПК. Вывести: type, model, speed.  */
-SELECT DISTINCT type, l.model, l.speed FROM laptop l INNER JOIN product ON l.model = product.model 
+SELECT DISTINCT type, l.model, l.speed FROM laptop l JOIN product p ON l.model = p.model 
 WHERE speed < (SELECT MIN(speed) FROM pc);
+/* или */
+SELECT DISTINCT type, l.model, l.speed FROM laptop l JOIN product p ON l.model = p.model
+WHERE l.speed < ALL(SELECT speed FROM pc);
 
 Задание № 18:
 /* Найдите производителей самых дешевых цветных принтеров. Вывести: maker, price. */
-SELECT DISTINCT maker, price  
-FROM printer prt JOIN product p ON prt.model= p.model  
+SELECT DISTINCT maker, price FROM printer prt JOIN product p ON prt.model= p.model  
 WHERE price = (select min(price)from printer where color = 'y') and color = 'y';
+/*или */ 
+SELECT DISTINCT maker, price FROM product, printer WHERE product.model = printer.model 
+AND color = 'y' AND price IN (SELECT min(price) FROM printer WHERE color ='y');
 
 Задание № 19:
 /* Для каждого производителя, имеющего модели в таблице Laptop, найдите средний размер экрана выпускаемых им ПК-блокнотов.
@@ -105,6 +110,54 @@ SELECT maker FROM product p INNER JOIN pc ON p.model = pc.model WHERE speed > = 
 INTERSECT
 SELECT maker FROM product p INNER JOIN laptop l ON p.model = l.model WHERE speed > = 750;
 
+Задание № 24:
+/* Перечислите номера моделей любых типов, имеющих самую высокую цену по всей имеющейся в базе данных продукции. */
+SELECT model FROM(
+       SELECT model, price FROM pc UNION
+       SELECT model, price FROM laptop UNION
+       SELECT model, price FROM printer) AS m
+WHERE price IN(
+       SELECT max(price) FROM(
+       SELECT price FROM pc UNION
+       SELECT price FROM laptop UNION
+       SELECT price FROM printer) AS m);
+       
+Задание № 26:
+/* Найдите среднюю цену ПК и ПК-блокнотов, выпущенных производителем A (латинская буква). Вывести: одна общая средняя цена. */
+SELECT avg(price) FROM (
+SELECT price FROM pc, product p WHERE pc.model = p.model AND maker = 'A'
+UNION ALL
+SELECT price FROM laptop l , product p WHERE l.model = p.model AND maker = 'A') AS avg
+
+Задание № 27:
+/* Найдите средний размер диска ПК каждого из тех производителей, которые выпускают и принтеры. 
+Вывести: maker, средний размер HD. */
+SELECT maker, avg(HD) AS Avg_HD FROM product p, pc WHERE p.model = pc.model AND maker IN (
+SELECT maker FROM product WHERE type = 'printer') GROUP BY maker;
+ 
+
+Задание № 28:
+/* Используя таблицу Product, определить количество производителей, выпускающих по одной модели. */
+SELECT count(maker) FROM (SELECT maker FROM product GROUP BY maker HAVING count(model) = 1) AS c;
+
+Задание № 35:
+/* В таблице Product найти модели, которые состоят только из цифр или только из латинских букв (A-Z, без учета регистра).
+Вывод: номер модели, тип модели. */
+SELECT model, type FROM product WHERE upper(model) not like '%[^A-Z]%'OR model not like '%[^0-9]%';
+
+Задание № 36:
+/* Перечислите названия головных кораблей, имеющихся в базе данных (учесть корабли в Outcomes). */
+SELECT name FROM ships WHERE class = name
+UNION 
+SELECT ship AS name FROM classes JOIN outcomes ON classes.class = outcomes.ship;
+
+Задание № 37:
+/* Найдите классы, в которые входит только один корабль из базы данных (учесть также корабли в Outcomes). */
+SELECT c.class FROM classes c JOIN (
+SELECT class,name FROM ships UNION
+SELECT c.class,o.ship FROM Outcomes o JOIN classes c ON c.class = o.ship) AS t1
+ON t1.class = c.class GROUP BY c.class HAVING COUNT(c.class) = 1;
+
 Задание № 38:
 /* Найдите страны, имевшие когда-либо классы обычных боевых кораблей ('bb') и имевшие когда-либо классы крейсеров ('bc'). */
 SELECT country FROM classes WHERE type = 'bb' AND country IN (SELECT country FROM classes WHERE type ='bc');
@@ -123,18 +176,49 @@ SELECT name FROM Ships WHERE name LIKE 'r%'
 UNION
 SELECT ship FROM Outcomes WHERE ship LIKE 'r%';
 
-Задание №:
-/* */
+Задание № 45:
+/* Найдите названия всех кораблей в базе данных, состоящие из трех и более слов (например, King George V).
+Считать, что слова в названиях разделяются единичными пробелами, и нет концевых пробелов. */
+SELECT name FROM ships WHERE name LIKE '% % %'
+UNION
+SELECT ship FROM outcomes WHERE ship LIKE '% % %'
+
+Задание № 48:
+/* Найдите классы кораблей, в которых хотя бы один корабль был потоплен в сражении. */
+SELECT class FROM classes, outcomes WHERE classes.class = outcomes.ship AND result = 'sunk'
+UNION
+SELECT class FROM ships, outcomes WHERE ships.name = outcomes.ship AND result = 'sunk';
+/* или */
+SELECT class FROM classes WHERE class IN(SELECT ship FROM outcomes WHERE result = 'sunk')
+UNION
+SELECT class FROM  ships WHERE name IN(SELECT ship FROM outcomes WHERE result ='sunk');
+
+Задание № 49:
+/* Найдите названия кораблей с орудиями калибра 16 дюймов (учесть корабли из таблицы Outcomes). */
+SELECT name FROM ships, classes WHERE ships.class = classes.class AND bore = '16'
+UNION 
+SELECT ship FROM outcomes, classes WHERE outcomes.ship = classes.class AND bore = '16';
+/* или */ 
+SELECT name FROM ships WHERE class IN(SELECT class FROM classes WHERE bore = '16')
+UNION 
+SELECT ship FROM outcomes WHERE ship IN(SELECT class FROM classes WHERE bore = '16');
 
 Задание №:
 /* */
-
 Задание №:
 /* */
-
 Задание №:
 /* */
-
+Задание №:
+/* */
+Задание №:
+/* */
+Задание №:
+/* */
+Задание №:
+/* */
+Задание №:
+/* */
 Задание №:
 /* */
 
